5c2340a6a54a9978f849b6c871f7bc575356831a
getleadconnect <getleadconnect@gmail.com>
2025-10-03 13:50:44 +0530

    changes

--- config/app.php
+++ config/app.php
@@ -65,8 +65,8 @@ return [
     |
     */
 
-    //'timezone' => 'UTC',
-    'timezone'=>'Asia/Kolkata',
+    'timezone' => 'UTC',
+    //'timezone'=>'Asia/Kolkata',
 
     /*
     |--------------------------------------------------------------------------
